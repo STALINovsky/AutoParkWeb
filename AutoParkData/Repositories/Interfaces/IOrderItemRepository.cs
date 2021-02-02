2e8d09caf9b44c8806c29b0700f4897a9817a04a
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-02 20:03:07 +0300

    Add Order Item Repository

---
+++ AutoParkData/Repositories/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoParkData.Model.Orders;
+
+namespace AutoParkData.Repositories.Interfaces
+{
+    public interface IOrderItemRepository
+    {
+        public Task<IEnumerable<OrderItem>> GetOrderItems();
+        public Task<OrderItem> Get(int id);
+        public Task Add(OrderItem orderItem, int orderId);
+        public Task Update(OrderItem item);
+        public Task Delete(int id);
+    }
+}
---
+++ AutoParkData/Repositories/OrderItemRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoParkData.Model.Orders;
+using AutoParkData.Repositories.Base;
+using AutoParkData.Repositories.Interfaces;
+using Dapper;
+
+namespace AutoParkData.Repositories
+{
+    public class OrderItemRepository : Repository, IOrderItemRepository
+    {
+        public OrderItemRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        private async Task<IEnumerable<OrderItem>> LoadOrderItems(string sql, object param = null)
+        {
+            return await Connection.QueryAsync<OrderItem, SparePart, OrderItem>(sql, (item, part) =>
+            {
+                item.SparePart = part;
+                return item;
+            }, param);
+        }
+
+        public Task<IEnumerable<OrderItem>> GetOrderItems()
+        {
+            const string sql = "SELECT oi.*, sp.* FROM OrderItems oi " +
+                               "Left JOIN SpareParts sp on oi.SparePartId = sp.Id ";
+            return LoadOrderItems(sql);
+        }
+
+        public async Task<OrderItem> Get(int id)
+        {
+            const string sql = "SELECT oi.*, sp.* FROM OrderItems oi " +
+                               "Left JOIN SpareParts sp on oi.SparePartId = sp.Id " +
+                               "Where oi.Id = @id";
+
+            return (await LoadOrderItems(sql, new { id })).Single();
+        }
+
+        public Task Update(OrderItem entity)
+        {
+            const string sql = "Update OrderItems Set " +
+                               "SparePartId = @SparePartId," +
+                               "Count = @Count " +
+                               "Where Id = @Id";
+
+            return Connection.ExecuteAsync(sql, new { SparePartId = entity.SparePart.Id, entity.Count, entity.Id });
+        }
+
+        public Task Delete(int id)
+        {
+            const string sql = "DELETE FROM OrderItems WHERE Id = @id";
+            return Connection.ExecuteAsync(sql, new { id });
+        }
+
+        public Task Add(OrderItem entity, int orderId)
+        {
+            const string sql = "INSERT INTO OrderItems (OrderId, SparePartId, Count) VALUES(@orderId, @sparePartId, @Count)";
+            return Connection.ExecuteAsync(sql, new { orderId, sparePartId = entity.SparePart.Id, entity.Count });
+        }
+    }
+}
