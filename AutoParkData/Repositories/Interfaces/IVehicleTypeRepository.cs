7f32c3c84e2b5bf4aed7e7f0ab0bc3e11384520c
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-02 19:37:53 +0300

    Add Vehicle type repository

---
+++ AutoParkData/Repositories/Interfaces/IVehicleTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParkData.Model.Vehicles;
+using AutoParkData.Repositories.Base;
+
+namespace AutoParkData.Repositories.Interfaces
+{
+    public interface IVehicleTypeRepository: IRepository<VehicleType>
+    {
+        Task<IEnumerable<VehicleType>> GetVehicleTypes();
+    }
+}
---
+++ AutoParkData/Repositories/VehicleTypeRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParkData.Model.Vehicles;
+using AutoParkData.Repositories.Base;
+using AutoParkData.Repositories.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace AutoParkData.Repositories
+{
+    public sealed class VehicleTypeRepository : Repository, IVehicleTypeRepository
+    {
+        public VehicleTypeRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<VehicleType> Get(int id)
+        {
+            return await Connection.QueryFirstAsync<VehicleType>("SELECT * FROM VehicleTypes WHERE Id = @id", new { id });
+        }
+
+        public async Task Add(VehicleType entity)
+        {
+            await Connection.ExecuteAsync("INSERT INTO VehicleTypes(TypeName, TaxCoefficient) VALUES(@TypeName, @TaxCoefficient)",
+                new { entity.TypeName, entity.TaxCoefficient });
+        }
+
+        public async Task Update(VehicleType entity)
+        {
+            await Connection.ExecuteAsync(
+                "UPDATE VehicleTypes SET TypeName = @TypeName, TaxCoefficient = @TaxCoefficient WHERE Id = @Id",
+                entity);
+        }
+
+        public async Task Delete(int id)
+        {
+            await Connection.ExecuteAsync("DELETE FROM VehicleTypes WHERE Id = @id", new { id });
+        }
+
+        public async Task<IEnumerable<VehicleType>> GetVehicleTypes()
+        {
+            return await Connection.QueryAsync<VehicleType>("SELECT * FROM VehicleTypes");
+        }
+    }
+}
