e8e1b10eed0c7888ef24a0fee1c2019e3b88049c
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-02 19:27:31 +0300

    Add base implementation of repository

---
+++ AutoParkData/Repositories/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using AutoParkData.Model.Base;
+
+namespace AutoParkData.Repositories.Base
+{
+    /// <summary>
+    /// Base CRUD methods for repository
+    /// </summary>
+    /// <typeparam name="T">Entity</typeparam>
+    public interface IRepository<T> where T : Entity
+    {
+        Task Add(T entity);
+        Task<T> Get(int id);
+        Task Update(T entity);
+        Task Delete(int id);
+    }
+}
---
+++ AutoParkData/Repositories/Base/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParkData.Model.Base;
+using Microsoft.Data.SqlClient;
+
+namespace AutoParkData.Repositories.Base
+{
+    /// <summary>
+    /// Base implementation of Repository, which contains db connection and realize IDisposable
+    /// </summary>
+    public abstract class Repository : IDisposable, IAsyncDisposable
+    {
+        /// <summary>
+        /// Ctor that init Sql connection
+        /// </summary>
+        /// <param name="connectionString">Connection string to DB</param>
+        protected Repository(string connectionString)
+        {
+            Connection = new SqlConnection(connectionString);
+        }
+        protected readonly DbConnection Connection;
+
+        public void Dispose()
+        {
+            Connection?.Dispose();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return Connection.DisposeAsync();
+        }
+    }
+}
