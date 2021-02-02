14ec6f45e77edefaab77343bdef629243cd0d5ad
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-02 18:23:48 +0300

    Add base for all entities Class

---
+++ AutoParkData/Model/Base/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParkData.Model.Base
+{
+    /// <summary>
+    /// Base class for all entities 
+    /// </summary>
+    public class Entity
+    {
+        public int Id { get; init; }
+    }
+}
