db5b8b86624aedcd0a979ca027548aaa775f843d
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-02 19:47:08 +0300

    Add Vehicle repository

---
+++ AutoParkData/Repositories/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParkData.Model.Vehicles;
+using AutoParkData.Repositories.Base;
+
+namespace AutoParkData.Repositories.Interfaces
+{
+    public interface IVehicleRepository : IRepository<Vehicle>
+    {
+        public Task<IEnumerable<Vehicle>> GetVehicles();
+    }
+}
---
+++ AutoParkData/Repositories/VehicleRepository.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParkData.Model.Vehicles;
+using AutoParkData.Repositories.Base;
+using AutoParkData.Repositories.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace AutoParkData.Repositories
+{
+    public class VehicleRepository : Repository, IVehicleRepository
+    {
+        public VehicleRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<IEnumerable<Vehicle>> GetVehicles()
+        {
+            return await Connection.QueryAsync<Vehicle, VehicleType, Vehicle>(
+                "SELECT * " +
+                "FROM Vehicles v " +
+                "LEFT JOIN VehicleTypes vt " +
+                "ON v.VehicleTypeId = vt.Id ", ((vehicle, vehicleType) =>
+                {
+                    vehicle.VehicleType = vehicleType;
+                    return vehicle;
+                }));
+        }
+
+        public async Task<Vehicle> Get(int id)
+        {
+            var query = await Connection.QueryAsync<Vehicle, VehicleType, Vehicle>(
+                @"SELECT * 
+                    FROM Vehicles v 
+                    JOIN VehicleTypes vt 
+                    ON v.VehicleTypeId = vt.Id 
+                    WHERE v.Id = @id",
+                (vehicle, type) =>
+                {
+                    vehicle.VehicleType = type;
+                    return vehicle;
+                }, new { id }
+            );
+
+            return query.Single();
+        }
+
+        public async Task Update(Vehicle entity)
+        {
+            await Connection.ExecuteAsync(
+                "UPDATE Vehicles SET " +
+                "ModelName = @ModelName, " +
+                "VehicleTypeId = @VehicleTypeId, " +
+                "Color = @Color, " +
+                "ManufactureYear = @ManufactureYear, " +
+                "Mileage = @Mileage, " +
+                "RegistrationNumber = @RegistrationNumber, " +
+                "Weight = @Weight, " +
+                "VolumeOfTank = @VolumeOfTank " +
+                "WHERE Id = @Id",
+                new
+                {
+                    entity.Id,
+                    entity.ModelName,
+                    VehicleTypeId = entity.VehicleType.Id,
+                    entity.Color,
+                    entity.ManufactureYear,
+                    entity.Mileage,
+                    entity.RegistrationNumber,
+                    entity.Weight,
+                    entity.VolumeOfTank
+                }
+            );
+        }
+
+        public async Task Delete(int id)
+        {
+            await Connection.ExecuteAsync("DELETE FROM Vehicles WHERE Id = @id", new { id });
+        }
+
+        public async Task Add(Vehicle entity)
+        {
+            await Connection.ExecuteAsync(
+                "INSERT INTO Vehicles " +
+                "(ModelName, VehicleTypeId, Color, ManufactureYear, Mileage, RegistrationNumber, Weight, VolumeOfTank, Consumption) " +
+                "VALUES (@ModelName, @VehicleTypeId, @Color, @ManufactureYear, @Mileage, @RegistrationNumber, @Weight, @VolumeOfTank, @Consumption) ",
+                new
+                {
+                    entity.ModelName,
+                    VehicleTypeId = entity.VehicleType.Id,
+                    entity.Color,
+                    entity.ManufactureYear,
+                    entity.Mileage,
+                    entity.RegistrationNumber,
+                    entity.Weight,
+                    entity.VolumeOfTank,
+                    entity.Consumption
+                }
+            );
+        }
+    }
+}
