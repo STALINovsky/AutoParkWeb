7cc3a49984593b0079edbed74d04bc8d0eb77ebd
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-04 00:28:49 +0300

    Add OrderItemViewModel

---
+++ AutoParkWeb/Models/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoParkData.Model.Orders;
+
+namespace AutoParkWeb.Models.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int OrderId { get; init; }
+        public OrderItem OrderItem { get; set; }
+    }
+}
