b7a6e8c4ab0a9eee1229cf596194c1042b99995b
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-06 07:35:17 +0300

    Add default Culture

--- AutoParkWeb/Program.cs
+++ AutoParkWeb/Program.cs
@@ -4,15 +4,25 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoParkWeb
 {
     public static class Program
     {
+        public static void SetCulture(string cultureName)
+        {
+            var culture = new CultureInfo(cultureName);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
         public static void Main(string[] args)
         {
+            SetCulture("en-US");
             CreateHostBuilder(args).Build().Run();
         }
 
